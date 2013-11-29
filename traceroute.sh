02be4f21bd6f2939f1f8abba6fceb7562b28b536
Philipp Winter <identity.function@gmail.com>
2013-11-29 16:55:10 +0100

    Add script to do TCP and ICMP traceroutes.
    The traceroutes are run to IP:port tuples contained in a file given over the
    command line.

---
+++ traceroute.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Copyright 2013 by Philipp Winter <phw@nymity.ch>
+
+if [ "$#" -lt 1 ]
+then
+	echo
+	echo "Usage: $0 <relay_list>"
+	echo
+	exit 1
+fi
+
+relaylist="$1"
+outdir="$(mktemp -d '/tmp/traceroutes-XXXXXX')"
+count=1
+all=$(wc -l $relaylist)
+all=(${all// / })
+all=${all[0]}
+
+timestamp() {
+	local file="$1"
+	echo "Date in UTC: $(date -u --rfc-3339=ns)" >> $file
+}
+
+for relay in $(cat $relaylist)
+do
+	# Parse the input which is in the format of IP:port.
+	tuple=(${relay//:/ })
+	ip=${tuple[0]}
+	port=${tuple[1]}
+	filebase="${outdir}/${ip}:${port}"
+
+	echo "[+] Beginning traceroutes ${count} of ${all} to ${ip}:${port}."
+	count=$((${count} + 1))
+
+	echo "[+] Running TCP traceroutes to ${ip}:${port}."
+	timestamp "${filebase}_tcp"
+	traceroute -T -A -O ack -n -p $port $ip >> "${filebase}_tcp" 2>&1
+
+	echo "[+] Running ICMP traceroutes to ${ip}:${port}."
+	timestamp "${filebase}_icmp"
+	traceroute -I -A -n $ip >> "${filebase}_icmp" 2>&1
+
+	echo "[+] Wrote results to \"${filebase}_{tcp,icmp}\"."
+done
