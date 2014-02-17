af96917e0c7d70686a9551f1be0f82f834916f10
Philipp Winter <phw@nymity.ch>
2014-02-17 13:23:52 +0100

    Initial import of config.sh

---
+++ config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Copyright 2014 Philipp Winter <phw@nymity.ch>
+
+# Should be "uncensored" for the machine outside the GFW.
+prober_type="censored"
+
+# The (spoofed) IP address of the censored machine behind the GFW.
+spoofed_addr=""
