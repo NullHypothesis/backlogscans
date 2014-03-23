dfb18131737e76626a1b1530c5772d44828a99e9
Philipp Winter <phw@nymity.ch>
2014-03-23 20:12:39 +0100

    Increase sending rate.

--- rstscan.sh
+++ rstscan.sh
@@ -53,18 +53,18 @@ then
 	iptables -A OUTPUT -d ${dst_addr} -p tcp --tcp-flags RST RST -j DROP
 
 	log "Sending ${control_syns} control TCP SYN segments to ${dst_addr}:${port}."
-	timeout 5 hping3-custom -n -c $control_syns -i u13000 -q -S -L 0 -s 20000 -p ${port} ${dst_addr} &
+	timeout 5 hping3-custom -n -c $control_syns -i u1300 -q -S -L 0 -s 20000 -p ${port} ${dst_addr} &
 
-	# 15,000 usec means ~66.7 SYNs a second.
+	# 6,000 usec means ~166.6 SYNs a second.
 	log "Sending ${probing_syns} spoofed TCP SYN segments to ${dst_addr}."
-	timeout 5 hping3-custom -n -c $probing_syns -a $spoofed_addr -i u13000 -q -S -L 0 -M 1000000 -s 30000 -p ${port} ${dst_addr} &
+	timeout 5 hping3-custom -n -c $probing_syns -a $spoofed_addr -i u6000 -q -S -L 0 -M 1000000 -s 30000 -p ${port} ${dst_addr} &
 else
-	# 15,000 usec means ~66.7 SYNs a second.
+	# 6,000 usec means ~166.6 SYNs a second.
 	log "Sending ${probing_syns} TCP SYN segments to ${dst_addr}."
-	timeout 5 hping3-custom -n -c $probing_syns -i u13000 -q -S -L 0 -M 1000000 -s 30000 -p ${port} ${dst_addr} &
+	timeout 5 hping3-custom -n -c $probing_syns -i u6000 -q -S -L 0 -M 1000000 -s 30000 -p ${port} ${dst_addr} &
 
 	# Wait a while to have some certainty that the SYNs made it to the target.
-	sleep 1
+	sleep 1.5
 
 	# Send RSTs which should reach the destination even though SYN/ACKs are blocked.
 	log "Sending ${probing_syns} TCP RST segments to ${dst_addr}."
