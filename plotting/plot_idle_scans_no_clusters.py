641c2fa30feb43195eee38e321c2c74ea3ba409f
Philipp Winter <phw@nymity.ch>
2014-04-16 16:27:30 -0600

    Add support for scan map clusters.

--- plotting/plot_idle_scans_no_clusters.py
+++ plotting/plot_idle_scans_no_clusters.py
@@ -2,11 +2,17 @@
 #
 # Copyright 2014 Philipp Winter <phw@nymity.ch>
 
+import csv
 import sys
+import os
 import argparse
 import logging
+import datetime
+import time
 
 import pygmaps
+
+LAT_LON_CSV = "gsIPs-lat-long.csv"
 
 handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter(fmt="%(asctime)s [%(levelname)s]: "
@@ -70,6 +76,11 @@ class Scan( object ):
 
         return (self.src_host, self.dst_host)
 
+    def get_ip_addresses( self ):
+
+        return (self.src_host.ip_addr,
+                self.dst_host.ip_addr)
+
     def __str__( self ):
 
         s = "Type %d at hour %d: %s --> %s" % (self.scan_verdict,
@@ -80,19 +91,39 @@ class Scan( object ):
 
 class Cluster( object ):
 
-    def __init__( self, cluster ):
+    def __init__( self, cluster_id ):
 
-        self.cluster = cluster
+        self.cluster_id = str(cluster_id)
         self.machines = []
 
     def add_machine( self, machine ):
 
         self.machines.append(machine)
 
+    def __iter__( self ):
+        return iter(self.machines)
+
     def __str__( self ):
 
-        return self.cluster + "\n\t" + \
+        return self.cluster_id + "\n\t" + \
                "\n\t".join([str(m) for m in self.machines])
+
+def get_lat_long( loc_data, ip_addr ):
+
+    fd = open(loc_data, 'r')
+    reader = csv.DictReader(fd,
+                            fieldnames = ['ip', 'lat', 'lon', 're', 'ipt'],
+                            delimiter = ',',
+                            quotechar = '"',
+                            skipinitialspace=True)
+
+    loc_data = list(reader)
+
+    for lats in loc_data:
+        if ip_addr in lats['ip']:
+            return [lats['lat'], lats['lon'], lats['re'], lats['ipt']]
+
+    return None
 
 def print_clusters( clusters, file_name ):
     """
@@ -108,7 +139,10 @@ def print_clusters( clusters, file_name ):
     # First, parse all scans and create dictionaries for the markers/points as
     # well as paths.
 
-    colors = ["#%06x" % (0xffffff / (5 * i)) for i in range(1, len(clusters)+1)]
+    colors = ["#ff0000", "#00ff00", "#0000ff", "#ffff00", "#00ffff",
+              "#ff00ff", "#000000", "#ffffff",
+              "#770000", "#007700", "#000077", "#777700", "#007777",
+              "#770077"]
 
     for cluster in clusters:
 
@@ -131,7 +165,7 @@ def print_clusters( clusters, file_name ):
                             color=color,
                             title=",".join(points[(latitude, longitude)]))
 
-    print "Writing output to \"%s\"." % file_name
+    logger.info("Writing output to \"%s\"." % file_name)
     my_map.draw(file_name)
 
 def print_map( scans, file_name ):
@@ -205,7 +239,7 @@ def print_map( scans, file_name ):
 
         my_map.addpath([src_coordinates, dst_coordinates], color=colour)
 
-    print "Writing output to \"%s\"." % file_name
+    logger.info("Writing output to \"%s\"." % file_name)
     my_map.draw(file_name)
 
 def parse_file( file_name ):
@@ -242,20 +276,31 @@ def parse_clusters( file_name ):
 
     clusters = []
     fd = open(file_name, 'r')
+    cluster_id = 0
 
     while True:
         line = fd.readline()
         if not line:
             break
 
-        # Format: <cluster>, <ip1>, <lat1>, <lon1>, ..., <ipn>, <latn>, <lonn>
+        # Format: <ip_1>, <ip_2>, ..., <ip_n>
 
-        elems = [elem.strip() for elem in line.split(',')]
-        cluster = Cluster(elems.pop(0))
+        ip_addrs = [ip_addr.strip() for ip_addr in line.split(',')]
+        cluster = Cluster(cluster_id)
+        cluster_id += 1
 
-        for i in xrange(0, len(elems), 3):
-            cluster.add_machine(Machine(elems.pop(0), float(elems.pop(0)),
-                                                      float(elems.pop(0))))
+        for ip_addr in ip_addrs:
+
+            ret = get_lat_long(LAT_LON_CSV, ip_addr)
+            if not ret:
+                logger.error("No location information for IP address %s." %
+                             ip_addr)
+                continue
+            else:
+                lat, lon = ret[:2]
+
+            cluster.add_machine(Machine(ip_addr, float(lat), float(lon)))
+
         clusters.append(cluster)
 
     return clusters
@@ -265,11 +310,11 @@ def parse_arguments( args ):
     parser = argparse.ArgumentParser(description="Plot and filter idle "
                                      "scan results on a clustered Google map.")
 
-    parser.add_argument("datafile", metavar="DATA_FILE",
+    parser.add_argument("datafile", metavar="IDLE_SCAN_FILE",
                         help="Parse and plot the given file.")
 
     parser.add_argument("-w", "--write", metavar="OUTPUT_FILE",
-                        type=str, default="scan_map.html",
+                        type=str, default="idle_scan_map.html",
                         help="Write HTML output to the given file.")
 
     parser.add_argument("-r", "--region", metavar="REGION",
@@ -295,11 +340,26 @@ def parse_arguments( args ):
                         help="Only display search result without printing "
                              "HTML/JavaScript.  Useful for manual analysis.")
 
-    parser.add_argument("-c", "--cluster",
-                        action="store_true",
-                        help="Create cluster from IP address file.")
+    parser.add_argument("-c", "--cluster", metavar="CLUSTER_FILE",
+                        type=str, help="Use the given cluster file.")
 
     return parser.parse_args()
+
+def mkdir_analysis():
+    """
+    Create and return a directory where all created data is stored.
+    """
+
+    dt = datetime.datetime.fromtimestamp(time.time())
+    dirname = dt.strftime("%Y-%m-%d_%H:%M:%S")
+
+    try:
+        os.mkdir(dirname)
+    except OSError as err:
+        logger.error("Could not create directory: %s" % err)
+        exit(1)
+
+    return dirname
 
 def main( ):
     """
@@ -308,14 +368,14 @@ def main( ):
 
     args = parse_arguments(sys.argv[0:])
 
-    logger.debug("Parsing file `%s'." % args.datafile)
+    dirname = mkdir_analysis()
 
     if args.cluster:
-        logger.debug("Attempting to create IP address clusters.")
-        clusters = parse_clusters(args.datafile)
-        print_clusters(clusters, args.write)
-        return 0
+        logger.debug("Parsing IP address cluster file `%s'." % args.cluster)
+        clusters = parse_clusters(args.cluster)
+        print_clusters(clusters, "%s/ip_addr_clusters.html" % dirname)
 
+    logger.debug("Parsing idle scan file `%s'." % args.datafile)
     scans = parse_file(args.datafile)
 
     # Filter scans based on the user's parameters.
@@ -354,8 +414,23 @@ def main( ):
         for scan in scans:
             print scan
     else:
-        print_map(scans, args.write)
+        print_map(scans, "%s/%s" % (dirname, args.write))
         logger.info("Wrote HTML data to `%s'." % args.write)
+
+    # Create cluster-specific scan maps.  Every scan in all scan maps contains
+    # an IP address which is part of a cluster.
+
+    for cluster in clusters:
+
+        cluster_scans = []
+
+        for machine in cluster:
+            for scan in scans:
+                if machine.ip_addr in scan.get_ip_addresses():
+                    cluster_scans.append(scan)
+
+        print_map(cluster_scans, "%s/cluster_%s.html" % (dirname,
+                                                         cluster.cluster_id))
 
     return 0
 
